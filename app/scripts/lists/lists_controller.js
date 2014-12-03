d59ee4ce1d02801c9160ebf55c1891996f922ca4
Chelsea Franz <chelseafranz3@gmail.com>
2014-12-03 12:28:18 -0500

    trying to add city param to each list

--- app/scripts/lists/lists_controller.js
+++ app/scripts/lists/lists_controller.js
@@ -6,20 +6,16 @@
 
 		MainFactory.getLists().success(function(data){
 			$scope.lists= data.results;
-			console.log($scope.lists);
-			list= $scope.lists;
-			console.log(list);
 		});
 
-		$scope.addList= function(newList, t){
-			console.log(t);
-			// ListsFactory.addList(newList);
-		//$('#lID').val(newList.objectId).trigger('input');
+		$scope.addList= function(newList, c){
+			console.log(c);
+			MainFactory.addList(newList);
+		$('#lID').val(newList.objectId).trigger('input');
 		
 			};
 
 		MainFactory.getOneCity($routeParams.id).success(function(data){
-        		console.log(data);
         		$scope.city=data;
         	});
 
