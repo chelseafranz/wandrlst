5eb381ccc02fa803efa98f34af67f162f498b69b
Chelsea Franz <chelseafranz3@gmail.com>
2014-12-07 19:33:02 -0500

    switched to mapbox

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 (function  () {
 
-	var app =angular.module('WanderMod', ['ngRoute', 'ngCookies', 'uiGmapgoogle-maps' ]);
+	var app =angular.module('WanderMod', ['ngRoute', 'ngCookies', 'uiGmapgoogle-maps',  'ui.utils' ]);
 		app.constant('PARSE_HEADERS', {
 			headers: {
         'X-Parse-Application-Id': 'iVBIZ8aBC1T1zcCOWvAc7AXDisgspY3S41YdI67u',
@@ -8,7 +8,7 @@
         'Content-Type': 'application/json'
       		}
 		});
-		app.constant('PARSE_URI', 'https://api.parse.com/1/')
+		app.constant('PARSE_URI', 'https://api.parse.com/1/');
 
 		app.config(function($routeProvider, uiGmapGoogleMapApiProvider){
 			
@@ -27,6 +27,7 @@
 			templateUrl: 'scripts/start/start.html',
 			controller: 'StartController'
 		});
+		
 		 $routeProvider.when('/city/:id', {
       		templateUrl: 'scripts/lists/lists-main.html',
       		controller: 'ListController'
@@ -37,6 +38,12 @@
 		 	controller: 'SearchController'
 		 });
 
+		 $routeProvider.when('/users/:uid', {
+		 	templateUrl: 'scripts/users/user-profile.html',
+		 	controller: 'StartController'
+		 });
+
+
 
 
 	}); //end route provider
@@ -44,17 +51,22 @@
 		// $( "#date" ).datepicker();
 
 
-    $( "#draggable" ).draggable();
+
+	
  
 
 	
 }());
 	 
 (function  () {
-	angular.module('WanderMod').factory( 'MainFactory', ['$http', 'PARSE_HEADERS', 'PARSE_URI',function ($http, PARSE_HEADERS, PARSE_URI){
+	angular.module('WanderMod').factory( 'MainFactory', ['$http', 'PARSE_HEADERS', 'PARSE_URI','$routeParams','$cookieStore',function ($http, PARSE_HEADERS, PARSE_URI, $routeParams, $cookieStore){
 		
-		var url= 'https://api.parse.com/1/classes/Cities';
-		var userUrl = ' https://api.parse.com/1/users';
+		var url= 'https://api.parse.com/1/classes/Cities/';
+		var userUrl = ' https://api.parse.com/1/users/';
+		var listUrl= 'https://api.parse.com/1/classes/Lists/';
+        var tipsUrl= 'https://api.parse.com/1/classes/tips/';
+        var dateUrl= 'https://api.parse.com/1/classes/date/';
+	   var brunchUrl= 'https://api.parse.com/1/classes/brunch/';
 
         var getCities = function () {
           return $http.get(url, PARSE_HEADERS);
@@ -62,7 +74,7 @@
 
         var addCity = function(city){
         $http.post(url, city, PARSE_HEADERS).success(function(){
-        	console.log('city has been added');
+            console.log('city added');
         });
         $('#addForm')[0].reset();
         };
@@ -71,9 +83,67 @@
         return $http.delete(url + cID, PARSE_HEADERS);
         };
 
-        var getOneCity= function(id){
-        return $http.get(url+ id, PARSE_HEADERS);
+        var getOneCity= function(cID){
+        return $http.get(url+ cID, PARSE_HEADERS);
         };
+
+         var tipsByCity = function (city) {
+            var query = '?'+'where={"city":"'+city+'"}';
+           return $http.get(tipsUrl + query, PARSE_HEADERS);
+          };
+
+          var listsByCity = function(city){
+            var query = '?'+'where={"city":"'+city+'"}';
+           return $http.get(listUrl + query, PARSE_HEADERS);
+          };
+
+        var datesByCity = function(city){
+            var query = '?'+'where={"city":"'+city+'"}';
+           return $http.get(dateUrl + query, PARSE_HEADERS);
+          };
+         var brunchsByCity = function(city){
+            var query = '?'+'where={"city":"'+city+'"}';
+           return $http.get(brunchUrl + query, PARSE_HEADERS);
+          };
+
+        var addList = function(list){
+		$http.post(listUrl, list, PARSE_HEADERS).success(function(){
+			console.log('successfully added');
+			});
+        //$('#bars')[0].reset();
+		};
+
+		
+		var getLists= function(){
+			return $http.get(listUrl, PARSE_HEADERS);
+		};
+
+        var addTip = function(tip){
+        $http.post(tipsUrl, tip, PARSE_HEADERS).success(function(){
+            console.log('successfully added a tip'); 
+        });
+         //$('#tips')[0].reset();
+        };
+
+        
+        var addDate = function(date){
+        $http.post(dateUrl, date, PARSE_HEADERS).success(function(){
+            console.log('successfully added a date');
+        });
+        //$('#date')[0].reset();
+        };
+
+        var addBrunch = function(brunch){
+        $http.post(brunchUrl, brunch, PARSE_HEADERS).success(function(){
+            console.log('successfully added a brunch');
+          });
+        //$('#brunch')[0].reset();
+        };
+
+
+
+
+
 
       
         return{
@@ -81,17 +151,28 @@
         	addCity : addCity,
         	deleteCity : deleteCity,
         	getOneCity : getOneCity,
+        	addList : addList,
+        	getLists : getLists,
+            addTip : addTip,
+            addDate : addDate,
+            addBrunch : addBrunch,       
+            tipsByCity : tipsByCity,
+            listsByCity : listsByCity,
+            datesByCity : datesByCity,
+            brunchsByCity : brunchsByCity
+
         	
         };
 }]);
 
 }());
 (function  () {
-	angular.module('WanderMod').factory( 'UserFactory', ['$http', 'PARSE_HEADERS', 'PARSE_URI','$location','$cookieStore', function ($http, PARSE_HEADERS, PARSE_URI, $location, $cookieStore){
+	angular.module('WanderMod').factory( 'UserFactory', ['$http', 'PARSE_HEADERS', 'PARSE_URI','$location','$cookieStore','$routeParams', function ($http, PARSE_HEADERS, PARSE_URI, $location, $cookieStore,$routeParams){
 
 		  var register = function (user) {
           $http.post(PARSE_URI+ 'users',user, PARSE_HEADERS).success( function (data){
             console.log('Welcome ' + user.username);
+            
           });
         };	
 
@@ -100,9 +181,7 @@
         $http.get(PARSE_URI + 'login/?'+params, PARSE_HEADERS)
             .success( function (data) {
             $cookieStore.put('currentUser', data);
-            console.log( 'welcome back '+ user.username);
-              return checkUser();
-              
+              return checkUser();    
           });
         };
 
@@ -120,13 +199,29 @@
         var logout = function(user){
            $cookieStore.remove('currentUser');
           return checkUser();
-        }
+          console.log('logged out');
+        };
+         var currentUser = function () {
+          return $cookieStore.get('currentUser');
+          $scope.current= 'currentUser';
+          console.log($scope.current);
+        };
+
+        var userProfile = function(uid){
+          return $http.get(url+uid, PARSE_HEADERS);
+          
+        };
+
+
 
         return{
         	register : register,
         	login : login,
         	checkUser : checkUser,
-          logout : logout
+          logout : logout,
+          currentUser : currentUser,
+          userProfile : userProfile
+          
 
         }
 
@@ -134,22 +229,44 @@
 }]);
 }());
 (function() {
-	angular.module('WanderMod').controller('UserController', ['$scope', '$location', 'PARSE_HEADERS','UserFactory','$http',function  ($scope, $location, PARSE_HEADERS, UserFactory, $http) {
+	angular.module('WanderMod').controller('UserController', ['$scope', '$location', 'PARSE_HEADERS','UserFactory','MainFactory', '$http','$cookieStore','$routeParams',function  ($scope, $location, PARSE_HEADERS, UserFactory, $http, $cookieStore, MainFactory, $routeParams ) {
 		
+      //$scope.user = UserFactory.currentUser();
+     // console.log($scope.user.objectId);
+
+
   		$scope.addUser= function(user){
   			UserFactory.register(user);
+        UserFactory.login(user);
   			$location.path('/start');
   		};
 
   		$scope.login = function(user){
-  			UserFactory.login(user);
-  			$location.path('/start')
+  			UserFactory.login(user)	
   		};
 
-  		$scope.logout = function(user){
-  			UserFactory.logout(user);
-  			$location.path('/');
-  		}
+
+
+  	
+  	
+      var checkUser = function (user) {
+          var user = $cookieStore.get('currentUser');
+          if(user) {
+            $('#user').html('Welcome back ' + user.username);
+            $location.path('/start');
+          } else {
+            console.log('not logged in')
+            $('#user').html('Please Log In');
+            $location.path('/');
+          }
+        };
+
+        var currentUser = function(){
+          MainFactory.currentUser();
+          console.log('user');
+        };
+       
+
 
 
 
@@ -157,14 +274,46 @@
 }());
 (function  () {
 	angular.module('WanderMod')
-	.controller( 'StartController', ['$scope', 'uiGmapGoogleMapApi', '$location', '$http', 'PARSE_HEADERS', 'MainFactory',function($scope, uiGmapGoogleMapApi, $location, $http, PARSE_HEADERS, MainFactory ){
-		
+	.controller( 'StartController', ['$scope', 'uiGmapGoogleMapApi', '$location', '$http', 'PARSE_HEADERS', 'MainFactory','$routeParams','$cookieStore', 'UserFactory','SaveFactory', function($scope, uiGmapGoogleMapApi, $location, $http, PARSE_HEADERS, MainFactory, $routeParams, $cookieStore, UserFactory, SaveFactory ){
+
+
 		MainFactory.getCities().success( function (data) {
           $scope.cities = data.results;
         });
 
+		// $scope.$watch('city.name', function (newVal) {
+  //         if (newVal) {
+  //           MainFactory.getCities(newVal.objectId).success( function (data) {
+  //             $scope.cities = data.results;
+  //           });
+
+  //         }
+  //       });
+
+
+        MainFactory.getLists().success(function(data){
+			$scope.lists= data.results;
+		});
+		
+		$scope.user = UserFactory.currentUser();
+		userID= $scope.user.objectId;
+	
+        $scope.userProfile= function  (user) {
+        	UserFactory.getUserProfile
+         return $cookieStore.get('currentUser');
+         
+        };
+
+        $scope.addSave= function(save){
+        SaveFactory.addSave(save);
+        $scope.save= save;
+        };
+        
+
         $scope.addCity = function (city) {
           MainFactory.addCity(city);
+          $scope.city= city.name;
+           console.log(city);
         };
 
         $scope.getOneCity= function(cid){
@@ -172,40 +321,189 @@
         	console.log(cid);
         };
 
-		$scope.map = { 
-			center: { latitude: 45, longitude: -80 },
-			zoom: 3,
-		};
-
-		uiGmapGoogleMapApi.then(function(maps){});
-
-		var url= 'https://api.parse.com/1/classes/Cities';
-
+        
+        
 		$scope.deleteCity=function(cID, index){
 		MainFactory.deleteCity(cID).success( function () {
             $scope.cities.splice(index, 1);		
         });
     	};
 
+    	$scope.logout=function(user){
+    		UserFactory.logout(user);
+    		console.log("logged out");
+    	};
+
+    	$scope.userProfile= function(userID){
+    		console.log($scope.user.objectId);
+    		userID= $scope.user.objectId;
+    		$location.path('/users/'+ userID);
+    	};
+
+    	$('#map_canvas').gmap();
+
+			// uiGmapGoogleMapApi.then(function(maps){
+			// 	$('#map_canvas').gmap({ 'center': new google.maps.LatLng(42.345573,-71.098326), 'callback': function() {
+   //     		 $('#map_canvas').gmap('addMarker', { 'position': new google.maps.LatLng(42.345573,-71.098326) } );
+			// 	}});
+
+			// });
+
+			
+
 	}]); //end controller
 
 }());
 (function  () {
 
-	angular.module('WanderMod').controller('ListController', ['$scope','PARSE_HEADERS','PARSE_URI', '$location','ListsFactory', function($scope, PARSE_HEADERS, PARSE_URI, $location, ListsFactory){
+	angular.module('WanderMod').controller('ListController', ['$scope','PARSE_HEADERS','PARSE_URI', '$location','MainFactory','$routeParams', 'UserFactory',function($scope, PARSE_HEADERS, PARSE_URI, $location, MainFactory, $routeParams, UserFactory){
+		var list;
 
-		ListsFactory.getLists().success(function(data){
-			$scope.lists= data.results;
-		});
+		var addSave= function  (save, u) {
+			console.log(u);
+			SaveFactory.addSave(save);
+		};
+	
+	$scope.user = UserFactory.currentUser();
+	userID= $scope.user.objectId;
+	console.log(userID);
 
-		$scope.addList= function(list){
-		$('#lID').val(list.objectId).trigger('input');
-			ListsFactory.addList(list);
+		$scope.tipsShow= false;
+		$scope.listShow= false;
+		$scope.dateShow= false;
+		$scope.brunchShow= false;
+
+	
+	$( ".lists" ).draggable({ addClasses: true});
+	$( ".tips" ).draggable({ addClasses: true });
+	$( ".date" ).draggable({ addClasses: true});
+	$( ".brunch" ).draggable({ addClasses: true });
+	$('li').droppable({ addClasses: true});
+
+	$( "ul.droptrue" ).sortable({
+      connectWith: "ul"
+    });
+
+		MainFactory.getOneCity($routeParams.id).
+		success(function(data){
+	        $scope.city=data;
+	        
+	        MainFactory.tipsByCity(data.name).success(function(data){
+	        	$scope.tips=data.results;
+	        });
+	        MainFactory.listsByCity(data.name).success(function(data){
+	        	$scope.lists=data.results;
+	        });
+	        MainFactory.datesByCity(data.name).success(function(data){
+	        	$scope.dates=data.results;
+	        });
+	        MainFactory.brunchsByCity(data.name).success(function(data){
+	        	$scope.brunchs=data.results;
+	        });
+
+	        });
+	
+
+		$scope.addList= function(newList, c){
+			console.log(c);
+			newList.city= c;
+			MainFactory.addList(newList);
+			  $('#bars')[0].reset();
+			};
+
+
+		$scope.addTip= function(newTip, c){
+			console.log(c);
+			newTip.city= c;
+			MainFactory.addTip(newTip);
+			 
 		};
 
+		$scope.addDate=function(newDate, c){
+			newDate.city=c;
+			MainFactory.addDate(newDate);
+		};
+
+		$scope.addBrunch=function(newBrunch, c){
+			newBrunch.city=c;
+			MainFactory.addBrunch(newBrunch)	
+		};
+		
+
+		$scope.chooseList= function(){
+			var choose= $('select').val();
+			console.log(choose);
+			switch (choose){
+				case 'tips':
+				$scope.tipsShow= true;
+				break;
+				case 'bars':
+				$scope.listShow= true;
+				break;
+				case 'date':
+				$scope.dateShow= true;
+				break;
+				case 'brunch':
+				$scope.brunchShow= true;
+				break;
+				
+			}
+		};
+
+		$scope.listByCity = function(){
+			MainFactory.listByCity();
+		};
+
+		$(document).ready(function() {
+
+    var selectedClass = 'ui-state-highlight',
+        clickDelay = 600,
+        // click time (milliseconds)
+        lastClick, diffClick; // timestamps
+
+    $("#draggable li")
+    .draggable({
+        revertDuration: 10,
+        // grouped items animate separately, so leave this number low
+        containment: '.demo',
+
+        start: function(e, ui) {
+            ui.helper.addClass(selectedClass);
+        },
+        // stop: function(e, ui) {
+        //     // reset group positions
+        //     $('.' + selectedClass).css({
+        //         top: 0,
+        //         left: 0
+        //     });
+        // },
+        drag: function(e, ui) {
+            // set selected group position to main dragged object
+            // this works because the position is relative to the starting position
+            $('.' + selectedClass).css({
+                top: ui.position.top,
+                left: ui.position.left
+            });
+        }
+    });
 
 
-	}]);
+    $("#droppable, #draggable").sortable().droppable({
+        drop: function(e, ui) {
+            $('.' + selectedClass).appendTo($(this)).add(ui.draggable) // ui.draggable is appended by the script, so add it after
+            .removeClass(selectedClass).css({
+                top: 0,
+                left: 0
+            });
+        }
+    });
+
+});
+
+	
+		
+
+	}]); //end controller
 }());
 
 
@@ -242,19 +540,22 @@
 	  			}
 			};
 
+			
+
 
 }]);
 
 }());
 (function  () {
-	angular.module('WanderMod').factory( 'ListsFactory', ['$http', 'PARSE_HEADERS', 'PARSE_URI','$location', function ($http, PARSE_HEADERS, PARSE_URI, $location){
+	angular.module('WanderMod').factory( 'ListsFactory', ['$http', 'PARSE_HEADERS', 'PARSE_URI','$location',function ($http, PARSE_HEADERS, PARSE_URI, $location){
 
-		var listUrl= 'https://api.parse.com/1/classes/Lists';
+		
 
 		var addList = function(list){
 		$http.post(listUrl, list, PARSE_HEADERS).success(function(){
 			console.log('successfully added');
-			$('#listOne')[0].reset();
+			console.log($scope.newCity);
+			
 		});
 		};
 
@@ -264,11 +565,11 @@
 
 		
 
-
-
 		return{
 			addList : addList,
 			getLists : getLists
+
+			
 		}
 		
 
@@ -276,3 +577,23 @@
 }]);
 }());
 
+
+(function  () {
+	angular.module('WanderMod').factory('SaveFactory', ['$http', 'PARSE_HEADERS', 'PARSE_URI','$routeParams','$cookieStore',function ($http, PARSE_HEADERS, PARSE_URI, $routeParams, $cookieStore){
+
+		var saveUrl = 'https://api.parse.com/1/classes/save/';
+
+		var addSave= function(save){
+			$http.post(listUrl, save, PARSE_HEADERS).success(
+				function(){
+        	console.log('city has been saved');
+		});
+		};
+
+
+		return{
+			addSave : addSave
+		}
+}]);
+
+}());
